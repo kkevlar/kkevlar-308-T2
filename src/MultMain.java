f21b5ec12ddb9afdc8037b788dc9b2c04630d293
Kevin Kellar <kellar.kevin@gmail.com>
2019-02-06 14:20:44 -0800

    added travis things

---
+++ src/MultMain.java
@@ -0,0 +1,12 @@
+
+public class MultMain {
+
+	public static void main(String[] args) 
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[0]);
+		
+		System.out.printf("Product: %d\n", a*b);
+	}
+
+}
